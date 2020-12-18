b8ba359189f0d4c538cd83bf80287bb88041cc2c
Felis1 <distoxi@gmail.com>
2020-12-18 22:55:53 +0200

    third bot express

--- bot.js
+++ bot.js
@@ -1,9 +1,13 @@
-const { Telegraf } = require('telegraf')
-
+const { Telegraf } = require('telegraf');
+const express = require('express');
+const expressApp = express()
 const bot = new Telegraf('1420857612:AAFpEx-R5FniN4OCBGBwxlpE-v0jAfKQOR4');
-bot.start((ctx) => {
-    ctx.reply('Send us the screenshot 📄 of your payment 💰 and wait for further instructions. We will send them in next few 🕐 hours. Thank you very much, and will see you on the Course soon!😻😽');
-    ctx.replyWithSticker('CAACAgIAAxkBAAKG5F_cbrrw__ecLqMM6mBqQ06l40fOAAIIAAPp2BMoePnrnGhu-XEeBA');
+const PORT = process.env.PORT || 3000;
+expressApp.listen(PORT, () => {
+    bot.start((ctx) => {
+        ctx.reply('Send us the screenshot 📄 of your payment 💰 and wait for further instructions. We will send them in next few 🕐 hours. Thank you very much, and will see you on the Course soon!😻😽');
+        ctx.replyWithSticker('CAACAgIAAxkBAAKG5F_cbrrw__ecLqMM6mBqQ06l40fOAAIIAAPp2BMoePnrnGhu-XEeBA');
+    });
 });
 
 bot.launch();
