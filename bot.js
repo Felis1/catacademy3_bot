fe2150e7ebc05477809759e7c73823455cf71d8e
Sviatoslav <43699040+Felis1@users.noreply.github.com>
2020-12-21 10:21:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf('1420857612:AAFpEx-R5FniN4OCBGBwxlpE-v0jAfKQOR4');
 bot.start((ctx) => {
-    ctx.reply('Send us the screenshot 📄 of your payment 💰 and wait for further instructions. We will send them in next few 🕐 hours. Thank you very much, and will see you on the Course soon!😻😽');
+    ctx.reply('Please send us a screenshot 📄 of your payment 💰 and your phone ☎️ number. We will contact you ASAP. Thank you very much, and we will see you in the course!😻😽');
     ctx.replyWithSticker('CAACAgIAAxkBAAKG5F_cbrrw__ecLqMM6mBqQ06l40fOAAIIAAPp2BMoePnrnGhu-XEeBA');
 });
 
